10a8f4ce1edf53f8ab632f761131c51c7e1138e5
Hung232006 <24107264@st.phenikaa-uni.edu.vn>
2025-11-28 17:10:01 +0700

    add vnpay

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
 class Config:
+    # Flask & Database
     SECRET_KEY = 'your-secret-key'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:CSDL2021@127.0.0.1/login'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-class Config:
+
+    # VNPay
     VNP_TMN_CODE = "YOUR_TMN_CODE"
     VNP_HASH_SECRET = "YOUR_SECRET_KEY"
     VNP_URL = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"
